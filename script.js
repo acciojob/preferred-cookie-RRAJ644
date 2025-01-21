d7710713a0aa525050e23f8765eeac7731dbf36b
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2025-01-21 20:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-//your JS code here. If required.
-let rootelem = document.querySelector(':root');
-let submitbtn=document.querySelector('[type="submit"]');
+let submitbtn = document.querySelector('[type="submit"]');
 
-submitbtn.addEventListener('click',savefunc);
+submitbtn.addEventListener('click', savefunc);
 
 function savefunc(event) {
-	event.preventDefault();
-	let fontsize=document.getElementById('fontsize').value;
-   let fontcolor=document.getElementById('fontcolor').value;
-	document.cookie='fontsize='+fontsize+';'+'max-age=10000000;';
-	document.cookie='fontcolor='+fontcolor+';'+'max-age=10000000;';
+  event.preventDefault();
+  let fontsize = document.getElementById('fontsize').value;
+  let fontcolor = document.getElementById('fontcolor').value;
 
-	if(document.cookie!=='')
-{
-	console.log("whihier93284")
-	let cookies=document.cookie.split(';');
-	let ans=false;
-	for (let i = 0; i < cookies.length; i++) {
+  // Save cookies
+  document.cookie = 'fontsize=' + fontsize + ';' + 'max-age=10000000;';
+  document.cookie = 'fontcolor=' + fontcolor + ';' + 'max-age=10000000;';
 
-		if (cookies[i].includes('fontsize')) {
-		  console.log(cookies[i] + "fre");
-		  ans=true;
-		}
-	}
-	if(ans)
-			{
-		console.log("whihier")
-		document.documentElement.style.setProperty('--fontsize',fontsize+"px");
-		document.documentElement.style.setProperty('--fontcolor',fontcolor);
-		console.log(document.cookie)
-	}
+  if (document.cookie !== '') {
+    console.log("Cookies saved");
+
+    let cookies = document.cookie.split(';');
+    let ans = false;
+
+    // Check for saved cookies
+    for (let i = 0; i < cookies.length; i++) {
+      if (cookies[i].includes('fontsize') && cookies[i].includes('fontcolor')) {
+        ans = true;
+      }
+    }
+
+    if (ans) {
+      console.log("Applying styles from cookies");
+
+      // Set CSS variables on the root element
+      document.documentElement.style.setProperty('--fontsize', fontsize + "px");
+      document.documentElement.style.setProperty('--fontcolor', fontcolor);
+
+      // Apply styles to body or specific elements
+      document.body.style.fontSize = fontsize + "px";
+      document.body.style.color = fontcolor;
+
+      console.log("Document styles updated");
+    }
+  }
 }
-}
-
